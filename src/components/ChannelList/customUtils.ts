e11bd2698355adcde162cdf1648e1b2f6239c96f
agitafirstawan <agitafirstawan26@gmail.com>
2022-04-29 13:04:50 +0700

    update channel preview

--- src/components/ChannelList/customUtils.ts
+++ src/components/ChannelList/customUtils.ts
@@ -2,6 +2,7 @@ import moment from 'moment';
 
 // eslint-disable-next-line import/prefer-default-export
 export const calculateTime = (time) => {
+  console.log(time, 'timeskip')
   if (time) {
     const now = moment();
     const utc = now;
@@ -10,37 +11,16 @@ export const calculateTime = (time) => {
     const hours = utc.diff(date, 'hours');
     const days = utc.diff(date, 'days');
     const weeks = utc.diff(date, 'weeks');
-
-    if (days >= 30) {
-        if(typeof date === 'string') {
-            return new Date(date).toLocaleDateString();
-        }
-        return ""
+    console.log(days, 'harinih')
+    if (days >= 31) {
+      return new Date(date).toLocaleDateString();
     }
-
-    if (minutes < 60) {
-      if (minutes === 0) {
-        return '1m ago';
-      }
-      return `${minutes}m ago`;
+    if(days < 31 && days > 6) {
+      return `${weeks}w ago`;;       
     }
-    if (hours < 24) {
-      return `${hours}h ago`;
+    if(days < 6) {
+      return `${days}d ago`      
     }
-
-    if (days < 2) {
-      return 'Yesterday';
-    }
-
-    if (days >= 2 && days <= 6) {
-      return `${days}d ago`;
-    }
-
-    if (days >= 7 && days <= 13) {
-      return '1w ago';
-    }
-
-    return `${weeks}w ago`;
   }
   return '';
 };
