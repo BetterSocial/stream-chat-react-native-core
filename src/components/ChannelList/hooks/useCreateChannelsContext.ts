fc91e169d6e9de61b7724d879dd98994afb0a8e5
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-02-06 16:37:23 +0700

    fix: fix sort channel

--- src/components/ChannelList/hooks/useCreateChannelsContext.ts
+++ src/components/ChannelList/hooks/useCreateChannelsContext.ts
@@ -109,7 +109,7 @@ export const useCreateChannelsContext = <
                 loadingChannels,
                 loadingNextPage,
                 refreshing,
-                additionalData
+               newChannel
             ],
         );
 
