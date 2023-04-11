9f15e8ca7c626e715c1bf99d9d139276719bca6e
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-04-11 19:50:21 +0700

    fix: fix sort

--- src/components/ChannelList/hooks/useCreateChannelsContext.ts
+++ src/components/ChannelList/hooks/useCreateChannelsContext.ts
@@ -66,7 +66,7 @@ export const useCreateChannelsContext = <
     if(!additionalData) {
         additionalData = []
     }
-    let newChannel = [...channels, ...additionalData].sort((a, b) => new Date(b.data.last_message_at) - new Date(a.data.last_message_at))
+    let newChannel = [...channels, ...additionalData].sort((a, b) => new Date(b.data?.last_message_at).getTime() - new Date(a.data?.last_message_at).getTime())
     const channelsContext: ChannelsContextValue<At, Ch, Co, Ev, Me, Re, Us> = useMemo(
         () => ({
             additionalFlatListProps,
